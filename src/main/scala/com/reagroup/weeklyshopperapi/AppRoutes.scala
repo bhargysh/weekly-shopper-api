41a4de52116ced987a007580d84e4865b9651472
Bharg Sharma <bharg.sharma@rea-group.com>
2020-04-08 12:53:56 +1000

    Change /hello endpoint to /recipe - Add implicit encoder for vector of type string

--- src/main/scala/com/reagroup/weeklyshopperapi/AppRoutes.scala
+++ src/main/scala/com/reagroup/weeklyshopperapi/AppRoutes.scala
@@ -2,10 +2,14 @@ package com.reagroup.weeklyshopperapi
 
 import cats.effect.IO
 import org.http4s.dsl.io._
-import org.http4s.{HttpRoutes}
+import org.http4s.{EntityEncoder, HttpRoutes}
+import org.http4s.circe.jsonEncoderOf
 
 object AppRoutes {
+  implicit private val vectorOfStringToJsonToEE: EntityEncoder[IO, Vector[String]] =
+    jsonEncoderOf[IO, Vector[String]]
+
   val routes: HttpRoutes[IO] = HttpRoutes.of {
-    case GET -> Root / "hello" => Ok("hello world")
+    case GET -> Root / "recipe" => Ok(Vector.empty[String])
   }
 }
