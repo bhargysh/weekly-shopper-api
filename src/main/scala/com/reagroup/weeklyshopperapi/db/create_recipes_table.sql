08ff2b67fe0fa42478ca5cc15556e47363e2ad98
Bharg Sharma <bharg.sharma@rea-group.com>
2020-04-09 11:32:48 +1000

    Script for creating recipes 🍲 table in db

---
+++ src/main/scala/com/reagroup/weeklyshopperapi/db/create_recipes_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE recipes(
+    id INT PRIMARY KEY,
+    type INT NOT NULL,
+    name VARCHAR NOT NULL,
+    ingredients VARCHAR NOT NULL,
+    instruction VARCHAR NOT NULL,
+    duration INT NOT NULL,
+    link VARCHAR,
+    image_link VARCHAR,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
