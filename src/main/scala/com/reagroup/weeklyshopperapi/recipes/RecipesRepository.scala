8f392edd85f4ba53cc1acc25ffe2dcf056a2cc3d
Bharg Sharma <bharg.sharma@rea-group.com>
2020-04-09 17:48:24 +1000

    Query db for all recipes

--- src/main/scala/com/reagroup/weeklyshopperapi/recipes/RecipesRepository.scala
+++ src/main/scala/com/reagroup/weeklyshopperapi/recipes/RecipesRepository.scala
@@ -1,5 +1,23 @@
 package com.reagroup.weeklyshopperapi.recipes
 
-class RecipesRepository() {
+import cats.effect.IO
+import com.reagroup.weeklyshopperapi.models.Recipe
+import doobie.util.transactor.Transactor
+import doobie.implicits._
 
+
+class RecipesRepository(dbxa: Transactor[IO]) {
+  def fetchRecipes(): IO[Vector[Recipe]] = {
+    RecipesRepository.fetchRecipesQuery().to[Vector].transact(dbxa)
+  }
+}
+
+object RecipesRepository {
+
+  private def fetchRecipesQuery(): doobie.Query0[Recipe] = {
+    sql"""
+      SELECT id, type, name, ingredients, instruction, duration, link, image_link, created_at, servings FROM recipes
+      """
+      .query[Recipe]
+  }
 }
