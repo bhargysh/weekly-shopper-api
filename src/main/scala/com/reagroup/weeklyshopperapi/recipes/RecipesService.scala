c3c7ab20d6b1efcc221bfd6ad92ceef30ae0ee22
Bharg Sharma <bharg.sharma@rea-group.com>
2020-04-10 19:04:17 +1000

    Remove redundant file 🚮

--- src/main/scala/com/reagroup/weeklyshopperapi/recipes/RecipesService.scala
+++
@@ -1,8 +0,0 @@
-package com.reagroup.weeklyshopperapi.recipes
-
-import cats.effect.IO
-import com.reagroup.weeklyshopperapi.models.Recipe
-
-class RecipesService(fetchRecipes: IO[Vector[Recipe]]) {
-  def getRecipes(): IO[Vector[Recipe]] = fetchRecipes
-}
