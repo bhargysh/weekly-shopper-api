404e30731bae50223c4eb51094f962f317c82ee9
Bharg Sharma <bharg.sharma@rea-group.com>
2020-04-10 17:35:26 +1000

    Reformat and declare case classes to be final

--- src/main/scala/com/reagroup/weeklyshopperapi/models/Recipe.scala
+++ src/main/scala/com/reagroup/weeklyshopperapi/models/Recipe.scala
@@ -5,29 +5,31 @@ import java.time.Instant
 import io.circe.{Encoder, Json}
 import io.circe.syntax._
 
-case class Recipe(id: RecipeId,
-                  category: Option[RecipeType],
-                  name: String,
-                  ingredients: String, // Vector[Ingredient] -> Ingredient(measurement: Measurement, rawIngredient: String)
-                  instructions: String, // Vector[Instruction] -> Instruction(step: Int, direction: String)
-                  duration: Int, //could be minutes or hours and minutes 🤔
-                  link: Option[String], // URL
-                  imageLink: Option[String], // URL
-                  createdAt: Instant,
-                  servings: Int)
-
+final case class Recipe(
+  id: RecipeId,
+  category: RecipeType,
+  name: String,
+  ingredients: String,
+  instructions: String,
+  duration: Int,
+  link: Option[String],
+  imageLink: Option[String],
+  createdAt: OffsetDateTime,
+  servings: Int
+)
 
 object Recipe {
-  implicit val encoder: Encoder[Recipe] = recipe => Json.obj(
-    "id" -> recipe.id.asJson,
-    "category" -> recipe.category.asJson,
-    "name" -> recipe.name.asJson,
-    "ingredients" -> recipe.ingredients.asJson,
-    "instructions" -> recipe.instructions.asJson,
-    "duration" -> recipe.duration.asJson,
-    "link" -> recipe.link.asJson,
-    "imageLink" -> recipe.imageLink.asJson,
-    "createdAt" -> recipe.createdAt.asJson,
-    "servings" -> recipe.servings.asJson,
-  )
+  implicit val encoder: Encoder[Recipe] = recipe =>
+    Json.obj(
+      "id" -> recipe.id.asJson,
+      "category" -> recipe.category.asJson,
+      "name" -> recipe.name.asJson,
+      "ingredients" -> recipe.ingredients.asJson,
+      "instructions" -> recipe.instructions.asJson,
+      "duration" -> recipe.duration.asJson,
+      "link" -> recipe.link.asJson,
+      "imageLink" -> recipe.imageLink.asJson,
+      "createdAt" -> recipe.createdAt.asJson,
+      "servings" -> recipe.servings.asJson
+    )
 }
--- src/main/scala/com/reagroup/weeklyshopperapi/models/RecipeId.scala
+++ src/main/scala/com/reagroup/weeklyshopperapi/models/RecipeId.scala
@@ -4,7 +4,7 @@ import doobie.util.meta.Meta
 import io.circe.Encoder
 import io.circe.syntax._
 
-case class RecipeId(rawInt: Int) extends AnyVal
+final case class RecipeId(rawInt: Int) extends AnyVal
 
 object RecipeId {
   implicit val recipeIdMeta: Meta[RecipeId] = Meta[Int].imap(RecipeId(_))(_.rawInt)
