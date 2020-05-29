28bff41356fe728c85783d0e8d02560f93421351
Bharg Sharma <bharg.sharma@rea-group.com>
2020-05-30 01:00:42 +1000

    Add encoder for custom errors

--- src/main/scala/com/reagroup/weeklyshopperapi/urls/saverecipe/RecipeValidationError.scala
+++ src/main/scala/com/reagroup/weeklyshopperapi/urls/saverecipe/RecipeValidationError.scala
@@ -1,5 +1,8 @@
 package com.reagroup.weeklyshopperapi.urls.saverecipe
 
+import io.circe.Encoder
+import io.circe.syntax._
+
 sealed trait RecipeValidationError
 
 case object FieldIsRequired extends RecipeValidationError
@@ -9,3 +12,12 @@ case object CategoryIsInvalid extends RecipeValidationError
 case object RecipeCookTimeIsInvalid extends RecipeValidationError
 
 case object RecipeServingAmountIsInvalid extends RecipeValidationError
+
+object RecipeValidationError {
+  implicit val encoder: Encoder[RecipeValidationError] = Encoder {
+    case FieldIsRequired => "This field is required".asJson
+    case CategoryIsInvalid => "This category is invalid".asJson
+    case RecipeCookTimeIsInvalid => "Invalid cook time".asJson
+    case RecipeServingAmountIsInvalid => "Invalid servings amount".asJson
+  }
+}
